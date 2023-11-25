ba15cccf1319bbdfadd0f3bf8f49181ec1d3ab4b
Eduardo Almada <56881337+almadaedu@users.noreply.github.com>
2023-11-24 21:15:36 -0300

    17-11-2023 integração projeto react Springboot

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Aside from './components/Aside'
+import Main from './components/Main'
+
+
+function App() {
+  return (
+  <html>
+    <Header/>
+    <Aside/>
+    <Main/>
+    <Footer/>
+  </html>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Aside.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Aside() {
+    return (
+        <p>Menu Esquerdo</p>
+    )
+}
+
+export default Aside
